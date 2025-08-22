71d98ab45fee844f553ccb492f6308b8416e24bb
Santhoshkondey11 <santhoshkondey03@gmail.com>
2025-08-22 16:15:49 +0530

    Add files via upload

---
+++ src/module-info.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module hospatalmanagement {
+	requires java.desktop;
+	requires java.logging;
+	requires java.sql;
+}
+
+
+/*
+module HospitalManagement {
+	requires java.desktop;
+	requires java.logging;
+	requires java.sql;
+}
+*/
